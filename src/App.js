c8325b2b2ef71dfce9178eabfd42bb79060ba240
Virenk21 <viren.d222111@gmail.com>
2022-03-19 05:09:01 +0530

    updated header needs to be completed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,189 @@
 import './App.css';
+import styled from "styled-components";
 
 function App() {
   return (
     <div className="app">
-    <div className="app__header">
-      <img
-        className="app__headerImage"
-        src="https://instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
-        alt=""      
-      />
-     </div>
-    </div>
-  );
-}
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="https://instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt=""
+        />
+        </div>
+        <container>
+          <content>
+            <Search>
+              <div>
+                <input type="text" placeholder="Search" />
+              </div>
+              <SearchIcon>
+                <img src="/image/search-icon.svg" alt="" />
+              </SearchIcon>
+            </Search>
+            <Nav>
+          <NavListWrap>
+            <NavList className="active">
+              <a>
+                <img src="/image/instahome.png" alt="" />
+                <span>Home</span>
+              </a>
+            </NavList>
+            <NavList>
+              <a>
+                <img src="/image/instamsg.png" alt="" />
+                <span>Messages</span>
+              </a>
+            </NavList>
+            <NavList>
+              <a>
+                <img src="/image/instapost.png" alt="" />
+                <span>post</span>
+              </a>
+            </NavList>
+            <NavList>
+              <a>
+                <img src="/image/instaexplore.png" alt="" />
+                <span>explore</span>
+              </a>
+            </NavList>
+            <NavList>
+              <a>
+                <img src="/image/heart.png" alt="" />
+                <span>heart</span>
+              </a>
+            </NavList>
+            </NavListWrap>
+          </Nav>
+          </content>
+        </container>
+       </div>
+     );
+};
+
+const Container = styled.div`
+  background-color: white;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.08);
+  left: 0;
+  padding: 0 24px;
+  position: fixed;
+  top: 0;
+  width: 100vw;
+  z-index: 100;
+`;
+
+const Content = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 0 auto;
+  min-height: 100%;
+  max-width: 1128px;
+`;
+
+const Search = styled.div`
+  opacity: 1;
+  flex-grow: 1;
+  position: relative;
+  & > div {
+    max-width: 280px;
+    input {
+      border: none;
+      box-shadow: none;
+      background-color: #eef3f8;
+      border-radius: 2px;
+      color: rgba(0, 0, 0, 0.9);
+      width: 218px;
+      padding: 0 8px 0 40px;
+      line-height: 1.75;
+      font-weight: 400;
+      font-size: 14px;
+      height: 34px;
+      border-color: #dce6f1;
+      vertical-align: text-top;
+    }
+  }
+`;
+
+const SearchIcon = styled.div`
+  width: 40px;
+  position: absolute;
+  z-index: 1;
+  top: 10px;
+  left: 2px;
+  border-radius: 0 2px 2px 0;
+  margin: 0;
+  pointer-events: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Nav = styled.nav`
+  margin-left: auto;
+  display: block;
+  @media (max-width: 768px) {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    background: white;
+    width: 100%;
+  }
+`;
+
+const NavListWrap = styled.ul`
+  display: flex;
+  flex-wrap: nowrap;
+  list-style-type: none;
+  .active {
+    span:after {
+      content: "";
+      transform: scaleX(1);
+      border-bottom: 2px solid var(--white, #fff);
+      bottom: 0;
+      left: 0;
+      position: absolute;
+      transition: transform 0.2s ease-in-out;
+      width: 100%;
+      border-color: rgba(0, 0, 0, 0.9);
+    }
+  }
+`;
+
+const NavList = styled.li`
+  display: flex;
+  align-items: center;
+  a {
+    align-items: center;
+    background: transparent;
+    display: flex;
+    flex-direction: column;
+    font-size: 12px;
+    font-weight: 400;
+    justify-content: center;
+    line-height: 1.5;
+    min-height: 52px;
+    min-width: 80px;
+    position: relative;
+    text-decoration: none;
+    span {
+      color: rgba(0, 0, 0, 0.6);
+      display: flex;
+      align-items: center;
+
+      @media (max-width: 768px) {
+        min-width: 70px;
+      }
+    }
+  }
+
+  &:hover,
+  &:active {
+    a {
+      span {
+        color: rgba(0, 0, 0, 0.9);
+      }
+    }
+  }
+`;
 
 export default App;
