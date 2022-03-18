8563df0244cba112624cf7ee6d374831bc4cc6f9
Virenk21 <viren.d222111@gmail.com>
2022-03-19 03:00:37 +0530

    updated logo header

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      Instagram-project
+    <div className="app">
+    <div className="app__header">
+      <img
+        className="app__headerImage"
+        src="https://instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+        alt=""      
+      />
+     </div>
     </div>
   );
 }
