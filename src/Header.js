e1491c22c0f4c517d6fa94f42a07ed10d067c1a0
Virenk21 <viren.d222111@gmail.com>
2022-03-30 19:36:11 +0530

    updated search bar

---
+++ src/Header.js
@@ -0,0 +1,81 @@
+import styled from "styled-components";
+
+import React from "react";
+
+const Header = () => {
+  return (
+    <Container>
+      <Content>
+        <Search>
+          <div>
+            <input type="text" placeholder="Search" />
+          </div>
+          <SearchIcon>
+            <img src="/image/search-icon.svg" alt="" />
+          </SearchIcon>
+        </Search>
+      </Content>
+    </Container>
+  );
+};
+
+
+const Container = styled.div`
+  background-color: white;
+  left: 31%;
+  padding: 0 24px;
+  position: fixed;
+  top: 2%;
+  width: 100vw;
+  
+`;
+
+const Content = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 0 auto;
+  min-height: 100%;
+  max-width: 1128px;
+  
+`;
+
+const Search = styled.div`
+  opacity: 1;
+  flex-grow: 1;
+  position: relative;
+  & > div {
+    max-width: 280px;
+    input {
+      border-style: none;
+      box-shadow: none;
+      background-color: #efefef;
+      border-radius: 7px;
+      color: rgba(0, 0, 0, 0.9);
+      width: 218px;
+      padding: 0 8px 0 40px;
+      line-height: 1.75;
+
+      font-weight: 400;
+      font-size: 14px;
+      height: 34px;
+      border-color: #dce6f1;
+      vertical-align: text-top;
+    }
+  }
+`;
+
+const SearchIcon = styled.div`
+  width: 40px;
+  position: absolute;
+  z-index: 1;
+  top: 10px;
+  left: 2px;
+  border-radius: 0 2px 2px 0;
+  margin: 0;
+  pointer-events: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default Header;
