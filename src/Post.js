33848f0d9fc9d197af730c3dd9162fc881abf2db
Virenk21 <viren.d222111@gmail.com>
2022-04-05 23:41:42 +0530

    post updated

--- src/Post.js
+++ src/Post.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import "./Post.css";
+import Avatar from "@mui/material/Avatar";
+
 function Post() {
   return (
-    <div>
-     
-<h3>username</h3>
-<img src="https://wallpapers.com/wallpapers/switzerland-landscape-painting-sb42zwr6hsytdv2u.html" alt=""/>
+    <div className="post">
+      <Avatar
+      className="post__avatar"
+      alt="Viren"
+      src="/static/images/avatar/1.jpg"
+      />
+      <h3>Username</h3>
+      <img
+        className="post__image"
+        src="https://wallpapers.com/wallpapers/switzerland-landscape-painting-sb42zwr6hsytdv2u.html"
+        alt=""
+      />
 
+      <h4 className="post__text">
+        <strong>VIREN....</strong> Keep going viren
+      </h4>
     </div>
-  )
+  );
 }
 
 export default Post
